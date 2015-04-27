7dd1d9e9327aaf58fcef8aafbd6186a287fe86e1
Daniel Lopez Murrieta <dannyx@gmail.com>
2015-04-26 18:49:03 -0700

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,11 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
+    flash.now[:notice]='Welcome!'
+    flash.now[:alert]='My Birthday is soon.'
+    flash.each do |key, value|
+      puts "<div class='" + key + "'>"+ value +"</div>"
+    end
   end
 
 end
